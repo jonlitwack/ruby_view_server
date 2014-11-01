06e5d946b60393b4c8db8a2ffb017b89a7a6b03e
Jonathan Litwack <jon@jonlitwack.com>
2014-11-01 15:28:06 -0400

    Turned the formula into an aggregate

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,11 @@
 require 'erb'
 
-x = 42
-template = ERB.new "The value of x is: <%= x %>"
+x = [42, 27, 89]
+y = 0
+x.each do |i|
+    y += i
+end
+erb_h2 = "<h2>"
+erb_block = "The aggregated value of x is: <%= y.to_s %>"
+template = ERB.new(erb_block)
 puts template.result(binding)
